89484f3f50d5d9344cfae4300ac2d8dcdd08dc3e
Vivek Amin <aminvivek123@gmail.com>
2018-10-07 19:26:15 -0700

    kubernetes horizontal pod autoscaler example

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/plain');
-res.end('Hello World\n');
+res.end('Hello World!!\n');
 });
 server.listen(port, () => {
  console.log(`Server running on port: ${port}`);
